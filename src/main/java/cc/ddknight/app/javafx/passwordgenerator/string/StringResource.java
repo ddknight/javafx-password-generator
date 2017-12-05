4148b9e0e3aa274af8467a23bcaef3a59d7cde6c
ddknight <cc199026@gmail.com>
2017-12-05 16:25:14 +0800

    author info update

--- src/main/java/cc/ddknight/app/javafx/passwordgenerator/string/StringResource.java
+++ src/main/java/cc/ddknight/app/javafx/passwordgenerator/string/StringResource.java
@@ -9,7 +9,7 @@ public class StringResource {
     public static final String VERSION_MAX          = "1";
     public static final String VERSION_MID          = "0";
     public static final String VERSION_MIN          = "0";
-    public static final String AUTH                 = "初日剣";
+    public static final String AUTH                 = "ddknight";
     public static final String PRI_STAGE_TITLE      = "随机密码生成器";
     public static final String PASSWORD_MESSAGE     = "随机密码:";
     public static final String BTN_PASSWORD_REFRESH = "刷新:";
